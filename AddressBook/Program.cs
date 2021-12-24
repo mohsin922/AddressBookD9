9c3adc88c07b0bff95c31d0b76a735dd9eba298f
unknown <mohsinpeer22@gmail.com>
2021-12-24 17:01:56 +0530

    Edit details of Contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -10,7 +10,7 @@ namespace AddressBook
         {
             public string FirstName { get; set; }
             public string LastName { get; set; }
-            public string Addressess { get; set; }
+            public string Address { get; set; }
             public string City { get; set; }
             public string State { get; set; }
             public string PhoneNumber { get; set; }
@@ -29,7 +29,7 @@ namespace AddressBook
             person.LastName = Console.ReadLine();
 
             Console.Write("Enter Address : ");
-            person.Addressess = Console.ReadLine();
+            person.Address = Console.ReadLine();
 
             Console.Write("Enter City : ");
             person.City = Console.ReadLine();
@@ -48,12 +48,12 @@ namespace AddressBook
 
             People.Add(person);
         }
-        public static void PrintCustomer(Person person)
+        public static void PrintContact(Person person)
         {
             Console.WriteLine("First Name: " + person.FirstName);
             Console.WriteLine("Last Name: " + person.LastName);
             Console.WriteLine("Phone Number: " + person.PhoneNumber);
-            Console.WriteLine("Address : " + person.Addressess);
+            Console.WriteLine("Address : " + person.Address);
             Console.WriteLine("City : " + person.City);
             Console.WriteLine("State : " + person.State);
             Console.WriteLine("ZipCode : " + person.ZipCode);
@@ -61,7 +61,77 @@ namespace AddressBook
             Console.WriteLine("EmailId: " + person.Email);
             Console.WriteLine("-------------------------------------------");
         }
-        public static void PeopleDetails()
+        public static void EditContact()
+        {
+            if (People.Count != 0)
+            {
+                Console.WriteLine("Enter the Name of contact to edit details:");
+                string Modified = Console.ReadLine();
+                foreach (var person in People)
+                {
+                    if (person.FirstName.ToUpper() == Modified.ToUpper())
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Enter the option to modify the Contact: ");
+                            Console.WriteLine("Enter 1 to Change First name ");
+                            Console.WriteLine("Enter 2 to Change Last name ");
+                            Console.WriteLine("Enter 3 to Change Phone Number ");
+                            Console.WriteLine("Enter 4 to Change Address ");
+                            Console.WriteLine("Enter 5 to Change City ");
+                            Console.WriteLine("Enter 6 to Change State ");
+                            Console.WriteLine("Enter 7 to Change Pincode ");
+                            Console.WriteLine("Enter 8 to Exit ");
+                            int Check = Convert.ToInt32(Console.ReadLine());
+                            switch (Check)
+                            {
+                                case 1:
+                                    Console.WriteLine("Enter the New First Name: ");
+                                    person.FirstName = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the New Last Name: ");
+                                    person.LastName = Console.ReadLine();
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter the New Phone Number: ");
+                                    person.PhoneNumber = Console.ReadLine();
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter the New Address: ");
+                                    person.Address = Console.ReadLine();
+                                    break;
+                                case 5:
+                                    Console.WriteLine("Enter the New City: ");
+                                    person.City = Console.ReadLine();
+                                    break;
+                                case 6:
+                                    Console.WriteLine("Enter the New State: ");
+                                    person.State = Console.ReadLine();
+                                    break;
+                                case 7:
+                                    Console.WriteLine("Enter the New Pin Code: ");
+                                    person.ZipCode = Console.ReadLine();
+                                    break;
+                                case 8:
+                                    return;
+
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter the valid name!");
+                    }
+
+                }
+
+
+            }
+        }
+        public static void ListingPeople()
         {
             if (People.Count == 0)
             {
@@ -69,10 +139,10 @@ namespace AddressBook
                 Console.ReadKey();
                 return;
             }
-            Console.WriteLine("Details of newly created Person in your address book:\n");
+            Console.WriteLine("Current Contact's details in your address book:\n");
             foreach (var person in People)
             {
-                PrintCustomer(person);
+                PrintContact(person);
             }
             Console.WriteLine("\nPress any key to continue.");
             Console.ReadKey();
@@ -83,7 +153,9 @@ namespace AddressBook
         {
 
             GetPerson();
-            PeopleDetails();
+            ListingPeople();
+            EditContact();
+            ListingPeople();
 
         }
     }
