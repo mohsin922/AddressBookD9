65bb9aa12feab5014dd93e19ccaed39dd0712b2b
peermohsin22 <peermohsin22@gmail.com>
2022-01-09 13:05:39 +0530

    Refactor to add multiple AddressBook

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace AddressBook
+{
+    class Contacts
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNunmber { get; set; }
+        public string eMail { get; set; }
+    }
+}
