70879b1e16cb9653fd1e3058c507e95da35dd420
peermohsin22 <peermohsin22@gmail.com>
2022-01-09 13:55:34 +0530

    view Person by City or State

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -9,17 +9,20 @@ namespace AddressBook
             Console.WriteLine("Welcome to Address book program");
 
             AddressBookDict.addAddressBook();
-            Console.WriteLine("\n");
-            AddressBookDict.ContactsDisplay();
-            Console.WriteLine("\n");
-            AddressBookDict.EditContact();
-            Console.WriteLine("\n");
-            AddressBookDict.ContactsDisplay();
-            Console.WriteLine("\n");
-            AddressBookDict.ContactsDisplay();
-            AddressBookDict.ContactsDisplay();
-            AddressBookDict.ContactsDisplay();
-            AddressBookDict.ContactsDisplay();
+            AddressBookDict.PersonSearch();
+
+            //AddressBookDict.addAddressBook();
+            //Console.WriteLine("\n");
+            //AddressBookDict.ContactsDisplay();
+            //Console.WriteLine("\n");
+            //AddressBookDict.EditContact();
+            //Console.WriteLine("\n");
+            //AddressBookDict.ContactsDisplay();
+            //Console.WriteLine("\n");
+            //AddressBookDict.ContactsDisplay();
+            //AddressBookDict.ContactsDisplay();
+            //AddressBookDict.ContactsDisplay();
+            //AddressBookDict.ContactsDisplay();
         }
     }
 }
